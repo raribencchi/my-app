cd90155c5173caba31655f6f5f16174f0ff412cc
Rashmi Aribenchi <83005622+raribencchi@users.noreply.github.com>
2023-05-09 18:13:54 +0800

    added default city name

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -26,12 +26,14 @@ export default function WeatherInfo(props) {
           </div>
         </div>
         <div className="col-6">
+        <div className="d-flex">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
+    </div>
     </div>
   );
 }
