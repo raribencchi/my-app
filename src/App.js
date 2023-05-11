2e7ed4354a41137fa1bd329e04df75f94714f3a6
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-05-12 00:28:09 +0800

    Updated app css

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Weather from "./Weather";
 
-
-
 export default function App() {
   return (
     <div className="App"> 
     <div className='Container'>
     <h1>React Weather App </h1>
-    <Weather defaultCity="Perth" />
+    <Weather defaultCity="Perth"/>
     <footer>
       This project is coded by <a href="https://ra-responsive-portfolio-v2.netlify.app"> Rashmi Aribenchi</a> and is {" "}
       <a href="https://github.com/raribencchi/my-app" target="blank"> Open-sourced on GitHub and hosted on Netlify.</a>
