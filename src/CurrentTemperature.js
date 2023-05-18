dce6b3401442f5379d289a7f9cd7d4888744db93
Rashmi Aribenchi <83005622+raribencchi@users.noreply.github.com>
2023-05-18 18:37:35 +0800

    Removed Fahrenheit

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -4,34 +4,19 @@ import React, { useState } from "react";
 export default function CurrentTemperature(props) {
     const [unit, setunit]=useState("celsius");
     
-    
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setunit("fahrenheit");
-    }
-
     function showCelsius(event) {
         event.preventDefault();
         setunit("celsius");
     }
-
-    if (unit=== "fahrenheit") {
+    
+ if (unit==="celsius") 
 
       return (
           <div className="CurrentTemperature">
-           <span className="temperature">{Math.round((props.celsius *9)/5+32)}</span>
-            <span className="unit">°C / {" "} <a href="/" onClick={showCelsius}>°F</a></span>
+           <span className="temperature">{Math.round(props.celsius)}</span>
+            <span className="unit">°C {" "} <a href="/" onClick={showCelsius}>
+              </a></span>
           </div>
-        );
-     
-
-    }else {
-      
-      return (
-        <div className="CurrentTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">°C / {" "} <a href="/" onClick={showFahrenheit} position="relative">°F </a></span>
-        </div>
-      );
-    }
- }
+        );       
+  }
+ 
